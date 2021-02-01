50ee5bb1fdbbfe365570aa0cb787da661ba3dd81
toquothty <34251613+toquothty@users.noreply.github.com>
2021-02-01 15:18:06 -0500

    Clean file formatting. Remove duplicate token process.

--- ACI_Scripts/APIC_InfoGathering.py
+++ ACI_Scripts/APIC_InfoGathering.py
@@ -4,13 +4,16 @@ import getpass
 import urllib3
 
 # Global variables pulling URL, username and password.
+# Initialize token which apic_login() function will fill to be passed.
 APIC_URL = "https://10.1.111.11/"
 username = input("USER: ")
 password = getpass.getpass("PASSWORD: ")
+token = ""
 
 def apic_login():
     """ Login to APIC """
 
+    global token
     token = ""
     err = ""
 
@@ -48,7 +51,7 @@ def apic_login():
 def get_tenants():
     """ Get Tenants """
 
-    token = apic_login()
+
     url = APIC_URL + "api/node/class/fvTenant.json"
     print('GET request resource: ', url)
 
@@ -64,7 +67,7 @@ def get_tenants():
 
         # Write Tenants to file in json format
         with open("C:\Python\ACI_Tenants.txt", "w") as outfile:
-            outfile.write('Response HTTP Status Code: {status_code}'.format(
+            outfile.write('Response HTTP Status Code: {status_code}\n'.format(
                 status_code=response.status_code))
             tenants = json.dumps(response.json(), indent=4)
             outfile.write(tenants)
@@ -76,7 +79,7 @@ def get_tenants():
 def get_devices():
     """ Get Devices """
 
-    token = apic_login()
+
     url = APIC_URL + "api/node/class/topology/pod-1/topSystem.json"
     print('GET request resource: ', url)
 
@@ -91,7 +94,7 @@ def get_devices():
 
         # Write ACI Devices to text in json format.
         with open("C:\Python\ACI_Devices.txt", "w") as outfile:
-            outfile.write('Response HTTP Status Code: {status_code}'.format(
+            outfile.write('Response HTTP Status Code: {status_code}\n'.format(
                 status_code=response.status_code))
             devices = json.dumps(response.json(), indent=4)
             outfile.write(devices)
@@ -102,7 +105,12 @@ def get_devices():
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+print('=========================LOGIN TO APIC=====================')
+apic_login()
 print('=========================GET TENANTS=======================')
 get_tenants()
 print('=========================GET DEVICES=======================')
 get_devices()
+
+# TODO: Pull all VPCs, total number of Up Links, be able to compare at quick glance
+# TODO: Pull OSPF instance/routes/number of routes/routing peers
